6b4e1016559b1b4351b72f95a979cc6633f33d23
Julius Markwei <julius.markwei@amalitech.com>
2024-11-15 13:39:47 +0000

    routers with view (template)

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,46 @@
-import http from "http";
+import http, { IncomingMessage, ServerResponse } from "http";
+import { readFile } from "./helper";
+import path from "path";
+
+const PORT = 3000;
+
+const requestHandler = (request: IncomingMessage, response: ServerResponse) => {
+    const { url, method } = request;
+    response.writeHead(200, { "Content-Type": "text/html" });
+    console.info(`${new Date().toISOString()} - ${method} ${url}`);
+
+    switch (url) {
+        case "/":
+            const pathToGreetingsFile = path.resolve(
+                __dirname,
+                "view",
+                "greetings.html"
+            );
+            const greet = readFile(pathToGreetingsFile);
+
+            response.write(greet);
+            break;
+        case "/users":
+            const pathToUsersFile = path.resolve(
+                __dirname,
+                "view",
+                "users.html"
+            );
+            const users = readFile(pathToUsersFile);
+
+            response.write(users);
+            break;
+        default:
+            const pathToErrorFile = path.resolve(__dirname, "view", "404.html");
+            const error = readFile(pathToErrorFile);
+
+            response.write(error);
+    }
+    response.end();
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(PORT, () => {
+    console.log("Server is running on port 3000");
+});
